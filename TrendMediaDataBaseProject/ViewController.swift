907ca9b3cd3170422f3efe280d6e7b8290f43245
wooseokJ <55547933+WooseokJ@users.noreply.github.com>
2022-08-04 09:01:31 +0900

    Initial Commit

---
+++ TrendMediaDataBaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrendMediaDataBaseProject
+//
+//  Created by useok on 2022/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
